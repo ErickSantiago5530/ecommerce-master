b619bc11a29d045bb9dd07af32ef50dcb281524e
Erick Santiago <santossantiagoerick@gmail.com>
2018-12-12 20:19:20 -0600

    Er 12.12.2018 20:19

---
+++ database/migrations/2018_12_12_001929_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id_producto');
+            $table->integer('id_usuario')->unsigned()->index();
+            $table->string('titulo');
+            $table->text('descripcion');
+            $table->decimal('precio',9,2);//centavos
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ resources/views/main/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title','Productos')
+
+@section('content')
+<h1>{{$name}}</h1>
+@endsection
